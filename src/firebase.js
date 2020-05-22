1b254fde660fcc5e8024e0ce11ee1ba8c33491f1
Aashish Peepra <ironhack082327@gmail.com>
2020-05-22 12:40:06 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 const Config = {
-    apiKey: "AIzaSyDYcHu7gViOEtC3ZQjHGsLZzslLH3avJxc",
-    authDomain: "theinitiatorsgame.firebaseapp.com",
-    databaseURL: "https://theinitiatorsgame.firebaseio.com",
-    projectId: "theinitiatorsgame",
-    storageBucket: "theinitiatorsgame.appspot.com",
-    messagingSenderId: "811314273977",
-    appId: "1:811314273977:web:0c7c1f69cb66a5af970568",
-    measurementId: "G-1KYT3G29CM"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
   // Initialize Firebase
   firebase.initializeApp(Config);
